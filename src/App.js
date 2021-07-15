8a1a821b46d6e7eec6cd1585765df1c573fc8034
Tal Yaniv <tal.yaniv@creditstacks.com>
2021-07-15 14:25:42 +0300

    added dotenv

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import env from 'react-dotenv';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Environment Variables: {JSON.stringify(process.env)}
+        Environment Variables: {JSON.stringify(env.TEST_V)}
       </header>
     </div>
   );
