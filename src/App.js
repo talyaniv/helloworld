cacf9040f38d3c2c7f9dcd84ee43ff194669b2e1
Tal Yaniv <tal.yaniv@creditstacks.com>
2021-07-15 15:41:06 +0300

    removed dotenv

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import env from 'react-dotenv';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Environment Variables: {JSON.stringify(env.TEST_V)}
+        Environment Variables: {JSON.stringify(process.env.REACT_APP_TEST_V)}
       </header>
     </div>
   );
